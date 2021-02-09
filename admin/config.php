98a73e915b6561fdae35331f07b3a7b934ed6486
João <desenvolvimento@orions.com.br>
2021-02-09 14:09:45 -0300

    initial commit

---
+++ admin/config.php
@@ -0,0 +1,36 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://weye.local/admin/');
+define('HTTP_CATALOG', 'http://weye.local/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://weye.local/admin/');
+define('HTTPS_CATALOG', 'http://weye.local/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/wamp64/www/weye/admin/');
+define('DIR_SYSTEM', 'C:/wamp64/www/weye/system/');
+define('DIR_IMAGE', 'C:/wamp64/www/weye/image/');
+define('DIR_STORAGE', 'C:/wamp64/www/storage_weye/');
+define('DIR_CATALOG', 'C:/wamp64/www/weye/catalog/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'weye');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
+
+// OpenCart API
+define('OPENCART_SERVER', 'https://www.opencart.com/');
