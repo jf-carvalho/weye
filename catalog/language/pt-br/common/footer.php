1dcd3cf552dfd317ae96a4f8f29f4b337002538b
João <desenvolvimento@orions.com.br>
2021-02-11 10:03:44 -0300

    footer links basic config

--- catalog/language/pt-br/common/footer.php
+++ catalog/language/pt-br/common/footer.php
@@ -1,15 +1,15 @@
 <?php
 // Text
-$_['text_information']  = 'Informações';
+$_['text_information']  = 'Institucional';
 $_['text_service']      = 'Serviços ao cliente';
-$_['text_extra']        = 'Outros serviços';
+$_['text_extra']        = 'Extras';
 $_['text_contact']      = 'Entre em contato';
 $_['text_return']       = 'Solicitar devolução';
 $_['text_sitemap']      = 'Mapa do site';
 $_['text_manufacturer'] = 'Produtos por marca';
-$_['text_voucher']      = 'Comprar vale presentes';
+$_['text_voucher']      = 'Vale presentes';
 $_['text_affiliate']    = 'Programa de afiliados';
-$_['text_special']      = 'Produtos em promoção';
+$_['text_special']      = 'Promoções';
 $_['text_account']      = 'Minha conta';
 $_['text_order']        = 'Histórico de pedidos';
 $_['text_wishlist']     = 'Lista de desejos';
